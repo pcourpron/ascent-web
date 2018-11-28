d5eb3c7ea3d94f5b7fa6db29b6b4623011821d59
pcourpron <pierrecourpron@gmail.com>
2018-11-28 11:53:19 -0800

    heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 // parse application/json
 app.use(bodyParser.json());
